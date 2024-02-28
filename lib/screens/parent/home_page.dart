c69df2cbf4e1a0c989e3acd9b894be15dec3c6c1
Edil-Ayehu <125575950+Edil-Ayehu@users.noreply.github.com>
2024-02-28 22:09:18 +0300

    Home page UI updated

--- lib/screens/parent/home_page.dart
+++ lib/screens/parent/home_page.dart
@@ -52,8 +52,9 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.lightBlueAccent,
+        backgroundColor: Colors.lightBlueAccent[100],
         foregroundColor: Colors.white,
+        elevation: 2,
         title: const Text(
           'Welcome',
           style: TextStyle(
@@ -154,7 +155,7 @@ class _HomePageState extends State<HomePage> {
                 _auth.signOut();
                 Navigator.pushReplacement(
                   context,
-                  MaterialPageRoute(builder: (context) => LoginPage()),
+                  MaterialPageRoute(builder: (context) => const LoginPage()),
                 );
               },
             ),
@@ -175,91 +176,159 @@ class _HomePageState extends State<HomePage> {
 
               // Display child details
               return Padding(
-                padding: const EdgeInsets.all(20.0),
+                padding: const EdgeInsets.all(10.0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Center(
-                      child: CircleAvatar(
-                        radius: 80.0,
-                        backgroundImage: NetworkImage(
-                            '${childData['imageUrl']}'), // You can replace this with an actual image
-                      ),
-                    ),
-                    const SizedBox(height: 15.0),
                     Card(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: Center(
-                          child: Text(
-                            '${childData['name']}',
-                            style: const TextStyle(
-                                fontSize: 32.0, fontWeight: FontWeight.bold),
+                      elevation: 4,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(14),
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(12),
+                          child: Image(
+                            image: NetworkImage('${childData['imageUrl']}'),
                           ),
                         ),
                       ),
                     ),
-                    const SizedBox(height: 50.0),
+                    const SizedBox(height: 5.0),
                     Text(
-                      'ABOUT ${childData['name']}'.toUpperCase(),
+                      '${childData['name']}',
                       style: const TextStyle(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold,
-                      ),
+                          fontSize: 32.0, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      '${childData['grade']} student',
+                      style: TextStyle(fontSize: 16.0,color: Colors.grey[400]),
                     ),
                     const SizedBox(height: 15.0),
                     Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        const Text(
-                          'Grade: ',
-                          style: TextStyle(
-                              fontSize: 18.0, fontWeight: FontWeight.bold),
+                        Card(
+                          elevation: 5,
+                          child: Container(
+                            width: MediaQuery.of(context).size.width * 0.44,
+                            height: 110,
+                            padding: const EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(8)),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Icon(
+                                  Icons.fitness_center,
+                                  size: 36,
+                                  color: Colors.lightBlueAccent,
+                                ),
+                                Text(
+                                  "${childData['weight']}kg",
+                                  style: const TextStyle(
+                                    fontSize: 28,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.lightBlueAccent,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
-                        Text(
-                          '${childData['grade']}',
-                          style: const TextStyle(fontSize: 18.0),
+                        Card(
+                          elevation: 5,
+                          child: Container(
+                            width: MediaQuery.of(context).size.width * 0.44,
+                            height: 110,
+                            padding: const EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(8)),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Icon(
+                                  Icons.straighten,
+                                  size: 36,
+                                  color: Colors.lightBlueAccent,
+                                ),
+                                Text(
+                                  "${childData['height']}m",
+                                  style: const TextStyle(
+                                    fontSize: 28,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.lightBlueAccent,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
                       ],
                     ),
                     const SizedBox(height: 10),
                     Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        const Text(
-                          'Age: ',
-                          style: TextStyle(
-                              fontSize: 18.0, fontWeight: FontWeight.bold),
+                        Card(
+                          elevation: 5,
+                          child: Container(
+                            width: MediaQuery.of(context).size.width * 0.44,
+                            height: 110,
+                            padding: const EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(8)),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Icon(
+                                  Icons.calendar_today,
+                                  size: 36,
+                                  color: Colors.lightBlueAccent,
+                                ),
+                                Text(
+                                  "${childData['age']} y/o",
+                                  style: const TextStyle(
+                                    fontSize: 28,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.lightBlueAccent,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
-                        Text(
-                          '${childData['age']} years',
-                          style: const TextStyle(fontSize: 18.0),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 10),
-                    Row(
-                      children: [
-                        const Text(
-                          'Height: ',
-                          style: TextStyle(
-                              fontSize: 18.0, fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          '${childData['height']} m',
-                          style: const TextStyle(fontSize: 18.0),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 10),
-                    Row(
-                      children: [
-                        const Text(
-                          'Weight: ',
-                          style: TextStyle(
-                              fontSize: 18.0, fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          '${childData['weight']} kg',
-                          style: const TextStyle(fontSize: 18.0),
+                        Card(
+                          elevation: 5,
+                          child: Container(
+                            width: MediaQuery.of(context).size.width * 0.44,
+                            height: 110,
+                            padding: const EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(8)),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Icon(
+                                  Icons.person,
+                                  size: 36,
+                                  color: Colors.lightBlueAccent,
+                                ),
+                                Text(
+                                  "${childData['gender']}",
+                                  style: const TextStyle(
+                                    fontSize: 28,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.lightBlueAccent,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
                       ],
                     ),
@@ -270,10 +339,13 @@ class _HomePageState extends State<HomePage> {
                           fontSize: 20.0, fontWeight: FontWeight.bold),
                     ),
                     const SizedBox(height: 10.0),
-                    ListTile(
-                      leading: const Icon(Icons.person),
-                      title: Text('${childData['parent name']}'),
-                      subtitle: Text('${childData['relationship']}'),
+                    Card(
+                      color: Colors.lightBlueAccent[100],
+                      child: ListTile(
+                        leading: const Icon(Icons.person,color: Colors.white,),
+                        title: Text('${childData['parent name']}',style: const TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
+                        subtitle: Text('${childData['relationship']}',style: const TextStyle(color: Colors.white),),
+                      ),
                     ),
                     const SizedBox(height: 20.0),
                     const Text(
@@ -282,15 +354,68 @@ class _HomePageState extends State<HomePage> {
                           fontSize: 20.0, fontWeight: FontWeight.bold),
                     ),
                     const SizedBox(height: 10.0),
-                    ListTile(
-                      leading: const Icon(Icons.phone),
-                      title: const Text('Phone'),
-                      subtitle: Text('${childData['p_phoneNo']}'),
-                    ),
-                    ListTile(
-                      leading: const Icon(Icons.email),
-                      title: const Text('Email'),
-                      subtitle: Text('${childData['p_email']}'),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Card(
+                          elevation: 5,
+                          child: Container(
+                            width: MediaQuery.of(context).size.width * 0.44,
+                            height: 110,
+                            padding: const EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(8)),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Icon(
+                                  Icons.phone,
+                                  size: 36,
+                                  color: Colors.lightBlueAccent,
+                                ),
+                                Text(
+                                  "${childData['p_phoneNo']}",
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.lightBlueAccent,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Card(
+                          elevation: 5,
+                          child: Container(
+                            width: MediaQuery.of(context).size.width * 0.44,
+                            height: 110,
+                            padding: const EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(8)),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Icon(
+                                  Icons.email,
+                                  size: 36,
+                                  color: Colors.lightBlueAccent,
+                                ),
+                                Text(
+                                  "${childData['p_email']}",
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.lightBlueAccent,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                     const SizedBox(height: 20.0),
                     const Text(
@@ -301,17 +426,17 @@ class _HomePageState extends State<HomePage> {
                     const SizedBox(height: 10.0),
                     ListTile(
                       leading: const Icon(Icons.medical_services),
-                      title: const Text('Allergies'),
+                      title: const Text('Allergies',style: TextStyle(fontWeight: FontWeight.bold,),),
                       subtitle: Text('${childData['allergies']}'),
                     ),
                     ListTile(
                       leading: const Icon(Icons.medical_services),
-                      title: const Text('Medical Conditions'),
+                      title: const Text('Medical Conditions',style: TextStyle(fontWeight: FontWeight.bold,),),
                       subtitle: Text('${childData['medical condition']}'),
                     ),
                     ListTile(
                       leading: const Icon(Icons.medical_services),
-                      title: const Text('Medications'),
+                      title: const Text('Medications',style: TextStyle(fontWeight: FontWeight.bold,),),
                       subtitle: Text('${childData['medications']}'),
                     ),
                     // Add more child details as needed
